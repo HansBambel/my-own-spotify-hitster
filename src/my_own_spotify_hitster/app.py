355ad5a79b4615d4eb8d370b1667d79912a4149d
Kevin <hans-bambel@gmx.net>
2024-08-31 18:24:12 +0200

    Enable play/pause of music

--- src/my_own_spotify_hitster/app.py
+++ src/my_own_spotify_hitster/app.py
@@ -6,40 +6,81 @@ from my_own_spotify_hitster.config import settings
 
 
 @st.cache_resource
-def get_spotify_read_auth():
-    """Get client from Spotify API to read library."""
+def get_spotify_client():
+    """Get client from Spotify API to read library and modify playback."""
     sp = spotipy.Spotify(
         auth_manager=SpotifyOAuth(
             client_id=settings.CLIENT_ID,
             client_secret=settings.CLIENT_SECRET,
             redirect_uri=settings.REDIRECT_URI,
-            scope="user-library-read",
+            scope=["user-read-playback-state", "user-modify-playback-state", "user-library-read"],
         )
     )
     return sp
 
 
-@st.cache_resource
-def get_spotify_modify_playback():
-    """Get client from Spotify API to modify playback."""
-    sp = spotipy.Spotify(
-        auth_manager=SpotifyOAuth(
-            client_id=settings.CLIENT_ID,
-            client_secret=settings.CLIENT_SECRET,
-            redirect_uri=settings.REDIRECT_URI,
-            scope="user-modify-playback-state",
-        )
-    )
-    return sp
+sp = get_spotify_client()
+
+
+def init_game():
+    """Initialize the game."""
+    if "player_count" not in st.session_state:
+        st.session_state.player_count = 2
+        reset_player_scores()
+    if "current_player" not in st.session_state:
+        st.session_state.current_player = 0
+    if "current_round" not in st.session_state:
+        st.session_state.current_round = 0
+
+
+def reset_player_scores():
+    """Reset the player scores."""
+    st.session_state.player_scores = {i: 0 for i in range(st.session_state.player_count)}
+
+
+def update_score_view():
+    """Update the score view."""
+    columns = st.columns(st.session_state.player_count)
+    for i, column in enumerate(columns):
+        column.write(f"Player {i + 1}")
+        column.write(f"Score: {st.session_state.player_scores[i]}")
 
 
 st.title("My Own Spotify Hitster")
+init_game()
 
-st.radio("Select music source", ["My Daily mix", "Some Playlist"])
-st.button("Start playing")
+# TODO maybe use a fragment here?
+with st.form("music_source"):
+    source = st.radio("Select music source", ["My Daily mix", "Some Playlist"])
+    st.form_submit_button("Submit")
+
+if source == "Some Playlist":
+    st.text_input("Playlist link", key="playlist_link")
 
 
-sp_read = get_spotify_read_auth()
+st.number_input("Number of players", min_value=1, value=2, step=1, on_change=reset_player_scores, key="player_count")
+# Create a score for each player containing the number of correct guesses
+update_score_view()
+
+st.header(f"Current Player: {st.session_state.current_player+1}")
+
+
+@st.fragment()
+def play_pause_music():
+    """Play/pause currently playing music."""
+    if "playing" not in st.session_state:
+        st.session_state.playing = True
+    clicked = st.button("Play/Pause")
+    if clicked:
+        if st.session_state.playing:
+            sp.pause_playback()
+        else:
+            sp.start_playback()
+        st.session_state.playing = False
+
+
+play_pause_music()
+
 
 # if __name__ == '__main__':
 # taylor_uri = 'spotify:artist:06HL4z0CvFAxyc27GXpf02'
--- src/my_own_spotify_hitster/config.py
+++ src/my_own_spotify_hitster/config.py
@@ -8,7 +8,7 @@ class Settings(BaseSettings):
 
     CLIENT_ID: str
     CLIENT_SECRET: str
-    REDIRECT_URI: str = "http://localhost:8501"
+    REDIRECT_URI: str = "http://localhost:8000"
 
 
 ROOT_DIR = Path(__file__).resolve().parent.parent
